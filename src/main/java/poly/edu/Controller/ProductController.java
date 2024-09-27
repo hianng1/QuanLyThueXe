0e47ac8322c17d9120edc7927c8284141a376028
prok13332 <name123091@gmail.com>
2024-09-27 11:43:11 +0700

    sua 1 chut

--- src/main/java/poly/edu/Controller/ProductController.java
+++ src/main/java/poly/edu/Controller/ProductController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
-	@GetMapping("/product")
+	@GetMapping("/car")
     public String getProductDetail() {
 
         return "/user/product_detail"; //
